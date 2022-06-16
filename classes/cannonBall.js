9e85df964ad7025a085dbda524b01c91ced8368e
victor-byjus <99813084+victor-byjus@users.noreply.github.com>
2022-06-16 10:10:07 -0300

    16/06/2022

--- classes/cannonBall.js
+++ classes/cannonBall.js
@@ -1,28 +1,58 @@
+//Cria a classe (molde) da bala de canhão
 class CannonBall {
+    //Define os parâmetros usados na criação de
+    //cada bala de canhão
     constructor(x,y){
+        //Define o tamanho do raio da bala de canhão
         this.r = 30;
+        //Configuração para a bala de canhão não cair
         var options = {
             isStatic: true
         }
+        //Cria um corpo circular para a bala de canhão
         this.body = Bodies.circle(x, y, this.r, options);
+        //Define a imagem
         this.image = loadImage("./assets/cannonball.png");
+        //Cria a matriz da trajetória
+        this.trajectory = [];
+        //Adiciona o corpo da bala de canhão ao mundo
         World.add(world,this.body);
     }
 
+    //Função para mostrar a bala de canhão
     display(){
+        //Cria uma variável para a posição do corpo
         var pos = this.body.position;
+
+        //Faz as configuraçöes da imagem da bala de canhão
         push();
         imageMode(CENTER);
         image(this.image, pos.x, pos.y, this.r, this.r);
         pop();
+
+        //Armazena as posicões da bala para a trajetória
+        if(this.body.velocity.x > 0 && pos.x > 10){
+            var position = [pos.x, pos.y];
+            this.trajectory.push(position);
+        }
+
+        //Desenha a trajetória da bala (rastro)
+        for(var i = 0; i < this.trajectory.length; i++){
+            image(this.image, this.trajectory[i][0],
+                  this.trajectory[i][1], 5, 5);
+        }
     }
 
+    //Função para atirar a bala de canhão
     shoot(){
+        //configurações do ângulo
         var newAngle = cannon.angle - 28;
         newAngle = newAngle * (3.14/180);
         var velocity = p5.Vector.fromAngle(newAngle);
         velocity.mult(0.5);
+        //Faz a bola poder se mover
         Matter.Body.setStatic(this.body, false);
+        //Adiciona velocidade à bola
         Matter.Body.setVelocity(this.body, {x: velocity.x * (180/3.14), y: velocity.y * (180/3.14)});
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,62 +1,105 @@
+//Configurações da biblioteca Matter
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world,ground;
+//Variáveis do nosso jogo
 var imagemDeFundo;
 var tower;
 var towerImagem;
 var cannon;
 var angle;
-var cannonBall
+var cannonBall;
+var balls = [];
 
+//Função que usamos para carregar os arquivos
 function preload() {
  imagemDeFundo = loadImage("./assets/background.gif");
  towerImagem = loadImage("./assets/tower.png");
 }
 
+//Função de configuração
 function setup() {
-
+  //Cria a tela
   canvas = createCanvas(1200, 600);
+  //Inicia o mundo com a biblioteca Matter
   engine = Engine.create();
   world = engine.world;
-  
- options={
- isStatic:true
- }
- 
- ground= Bodies.rectangle(0,height-1, width*2,1,options);
- World.add(world,ground);
+  //Configuração para a torre e o chão não caírem
+  options={
+    isStatic:true
+  }
 
- tower = Bodies.rectangle(160,350,160,310,options);
- World.add(world,tower);
+  //Crie o corpo para o chão
+  ground= Bodies.rectangle(0,height-1, width*2,1,options);
+  //Adiciona o corpo do chão ao mundo
+  World.add(world,ground);
 
- angleMode(DEGREES);
- angle = 20;
- cannon = new Cannon(180, 110, 130, 100, angle);
+  //Cria o corpo para a torre
+  tower = Bodies.rectangle(160,350,160,310,options);
+  //Adiciona o corpo da torre ao mundo
+  World.add(world,tower);
 
- cannonBall = new CannonBall(cannon.x, cannon.y);
+  //Define o modo do ângulo em graus
+  angleMode(DEGREES);
+  //Define o valor do ângulo
+  angle = 20;
+  //Cria o objeto de canhão com base na classe de Canhão
+  cannon = new Cannon(180, 110, 130, 100, angle);
  
 }
 
+//Função de desenho
 function draw() {
+  //Define a cor de fundo como cinza
   background(189);
+  //Coloca a imagem de fundo do jogo
   image(imagemDeFundo,0,0,1200,600);
+
+  //Faz a física do jogo ser atualizada
   Engine.update(engine);
  
- rect(ground.position.x, ground.position.y,width*2,1);
+  //Desenha o chão do jogo
+  rect(ground.position.x, ground.position.y,width*2,1);
   
- push();
- imageMode(CENTER);
- image(towerImagem, tower.position.x, tower.position.y, 160, 310);
- pop();
+  //Faz a configuração desejada ser apenas para a torre
+  push();
+  //Define o modo da imagem como centralizada
+  imageMode(CENTER);
+  //Desenha a imagem da torre
+  image(towerImagem, tower.position.x, tower.position.y, 160, 310);
+  //Termina a configuração da torre
+  pop();
+  
+  //Mostra o canhão
+  cannon.display();
 
- cannon.display();
- cannonBall.display();
+  //Mostra cada bala do canhão
+  for(var i = 0; i < balls.length; i++){
+    showCannonBalls(balls[i],i);
+  }
+
 }
 
+//Função que atira cada bala de canhão
 function keyReleased(){
   if(keyCode === DOWN_ARROW){
-    cannonBall.shoot();
+    balls[balls.length-1].shoot();
+  }
+}
+
+//Função que cria cada bala de canhão
+function keyPressed(){
+  if(keyCode === DOWN_ARROW){
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    balls.push(cannonBall);
+  }
+}
+
+//Função que é chamada para mostrar cada bala de canhão
+function showCannonBalls(ball, i){
+  if(ball){
+    ball.display();
   }
 }
